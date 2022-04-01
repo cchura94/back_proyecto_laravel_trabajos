99c86cc19fe526dca52f65f0ce5f677e1e330d04
cchura94 <cchura.cpc@gmail.com>
2022-03-31 22:04:49 -0400

    add rutas /

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get("/", function(){
+    return response()->json(["mensaje" => "Saludos desde la api"]);
+});
+
 Route::post("/v1/auth/login", [AuthController::class, "login"]);
 Route::post("/v1/auth/registro", [AuthController::class, "registro"]);
 
